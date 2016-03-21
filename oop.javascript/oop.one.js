7c387eafd6df735cc642b07505204dc3b115bf69
Byrdann Fox <mobileupstate@gmail.com>
2016-03-21 07:39:38 -0400

    fizz tut for student

--- oop.javascript/oop.one.js
+++ oop.javascript/oop.one.js
@@ -13,8 +13,9 @@ for (i = 1; i <= 100; i++) {
 // Prove you know how to declare a function and understand you can pass arguments to it
 function fizzBuzzer(n) {
 	// Prove you can use conditional statements to provide functionality based on mathematical return values (Prove you can do some arithmetic, too)
+	// Prove you know why you must check for "FizzBuzz" first
 	if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
 	if (n % 3 === 0) return "Fizz";
     if (n % 5 === 0) return "Buzz";
-	return n.toString(); // Prove you know that JavaScript is Object-Oriented
+	return n.toString(); // Prove you know that JavaScript is Object-Oriented, why you need alter the type of a value in this program
 }
