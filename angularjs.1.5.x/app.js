e495d6e28f97b412644ff59672abc496c59d0c2e
Byrdann Fox <mobileupstate@gmail.com>
2016-03-23 18:51:59 -0400

    some angular play

---
+++ angularjs.1.5.x/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var app = angular.module("collectionBindingApp", []);
+
+app.controller("EmployeeCtrl", ["$scope", function ($scope) {
+	var Employee = function(name, age) {
+		this.name = name; 
+		this.age = age;
+	};
+	
+	var getEmployees = function() {
+		return [
+			new Employee("Ryan Poplin", 25),
+			new Employee("Byrdann Fox", 26),
+			new Employee("Vredes Byrda", 35)
+		];
+	};
+	
+	$scope.employeeData = {
+		employees: getEmployees()
+	};
+}]);
