9cd9e7c4aa8534e4c49639f82e040b10c4dc5509
Byrdann Fox <byrdannfox@gmail.com>
2016-03-30 11:58:05 -0400

    asdfjkl;

--- angularjs.1.5.x/test.app.js
+++ angularjs.1.5.x/test.app.js
@@ -4,10 +4,11 @@
 
 // paste in your Google Chrome JavaScript console when watching a live feed...
 // currently, you cannot post more than 3 comments at a time, the 4th will be removed...
-// so the algo must say every fourth comment must come from a different IP address, or from a different user?
 const textBox = document.getElementById("live-comments-input-field");
-const commentArray = ["Here's", "Here's my", "Here's my comment..."];
 const submitButton = document.querySelectorAll(".post-button");
+
+const commentArray = ["Here's", "Here's my", "Here's my comment..."];
+
 var index;
 // 4: Message not sent: You've been sending too many messages. Take a break and try again.
 for (index = 0; index <= 3; ++index) {
@@ -17,3 +18,5 @@ function addComment(index) {
 	textBox.textContent = commentArray[index];
 	submitButton[0].click();
 }
+
+// <kirkg@laurenselectric.com>
